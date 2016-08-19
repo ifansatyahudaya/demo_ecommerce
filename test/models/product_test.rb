7d1c495d313b6cdd9b66e6ee18dfe11e32804966
ifan <ifansatyahudaya@gmail.com>
2016-08-19 13:33:53 +0700

    Commit Pertama

---
+++ test/models/product_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class ProductTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+    test "product price must be positive" do
+    product = Product.new(title:
+    "My Book Title",
+    description: "yyy",
+    "zzz.jpg")
+    image_url:
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+    product.errors[:price]
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+    product.errors[:price]
+    product.price = 1
+    assert product.valid?
+    end
+end
